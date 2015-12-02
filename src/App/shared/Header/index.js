9f24bade6ec4a1efaef6ff4afd4ebd17c9fe2326
Daniel Falabella <dnlfala@gmail.com>
2015-12-01 19:31:49 -0700

    Added link to login page

--- src/App/shared/Header/index.js
+++ src/App/shared/Header/index.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
 import CSSModules from 'react-css-modules'
 import s from './Header.css'
 
@@ -9,7 +10,7 @@ export default class Header extends React.Component {
         <div className={s.header}>
           <div className={s.left}></div>
           <div className={s.center}>HIGH CLUB</div>
-          <div className={s.right}></div>
+          <div className={s.right}><Link to={'/login'}>Login</Link></div>
         </div>
       </div>
     )
